84f65ba4b16a24e51f35ae7ba8134f993638aeff
070jhz <070jhz@proton.me>
2025-01-01 09:37:28 +0100

    add sensor components

--- include/MainFrame.h
+++ include/MainFrame.h
@@ -1,18 +1,25 @@
 #pragma once
+#include <wx/event.h>
 #include <wx/wx.h>
 
 // main window class
 
 class MainFrame : public wxFrame {
 public:
-  MainFrame();
+    MainFrame();
 
 private:
-  // event handlers
-  void onExit(wxCommandEvent &event);
-  void onAbout(wxCommandEvent &event);
+    // event handlers
+    void onExit(wxCommandEvent &event);
+    void onAbout(wxCommandEvent &event);
 
-  // gui elements
-  wxMenuBar *m_menuBar;
-  wxStatusBar *m_statusBar;
+    // gui elements
+    wxMenuBar *m_menuBar;
+    wxStatusBar *m_statusBar;
+    wxPanel *leftPanel; // sensor list
+    wxPanel *rightPanel; // sensor data
+    wxButton* m_connectBtn;
+    wxListBox* m_sensorList;
+    
+    DECLARE_EVENT_TABLE();
 };
